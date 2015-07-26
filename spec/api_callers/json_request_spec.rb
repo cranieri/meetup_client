c68dd379e7a799db3d2f6f15c52db2f1acbb6e5e
Cosimo Ranieri <engagedev1@gmail.com>
2015-07-26 20:49:09 +0200

    refactor test: remove tests of private method

--- spec/api_callers/json_request_spec.rb
+++ spec/api_callers/json_request_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 require 'net/http'
 
 describe ::ApiCallers::JsonRequest do
-  subject { ::ApiCallers::JsonRequest.new('') }
+  subject { ::ApiCallers::JsonRequest.new(response_body) }
+  let(:response_body) do
+    '{ "response": "I\'m the \"Best\"" }'
+  end
 
-  describe "#clean_response_body" do
-    let(:body_response) { 'I\'m the \"Best\"' }
-
-    it "removed \" from the passed text " do
-      expect(subject.send(:clean_response_body, body_response)).to eq 'I\'m the Best'
+  describe "#format_response" do
+    let(:expected_json) do
+      {"response" => 'I\'m the "Best"'}
     end
 
-    it "forces the encoding to utf-8" do
-      body_response.encode!('windows-1251')
-      expect(subject.send(:clean_response_body, body_response).encoding.to_s).to eq 'UTF-8'
+    it "formats the response from meetup" do
+      expect(subject.format_response(response_body)).to eq expected_json
     end
   end
 end
