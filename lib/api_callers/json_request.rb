39fdfd94134815fab4c7708cffea165612487d8c
Cosimo Ranieri <engagedev1@gmail.com>
2015-07-26 20:44:17 +0200

    get rid of active controller and active support dependencies

--- lib/api_callers/json_request.rb
+++ lib/api_callers/json_request.rb
@@ -1,15 +1,10 @@
 require 'api_callers/http_request'
+require 'json'
 
 module ApiCallers
   class JsonRequest < HttpRequest
     def format_response(response_body)
-      ActiveSupport::JSON.decode(clean_response_body(response_body))
-    end
-
-    private
-
-    def clean_response_body(response_body)
-      ActionController::Base.helpers.strip_tags(response_body.gsub('\"','').force_encoding(CHARSET))
+      JSON.parse(response_body)
     end
   end
 end
